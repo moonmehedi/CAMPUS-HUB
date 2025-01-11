357991670165c2911d5d8e02d460435d82b520bb
rubayetkabirzisan <rubayetkabirz@gmail.com>
2025-01-11 23:24:24 +0600

    "modified scholarship"

--- campus-hub/src/app/Users/Student/Scholarship/page.tsx
+++ campus-hub/src/app/Users/Student/Scholarship/page.tsx
@@ -5,17 +5,22 @@ import "../Scholarship/scholarship-application.css"
 
 export default function ScholarshipApplicationPage() {
   return (
-    <div className="scholarship-application-container">
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 pl-64">
-          <Header />
-          <div className="scholarship-application-content">
-            <ScholarshipApplicationContent />
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
+          <div className="scholarship-application-container">
+            <div className="scholarship-application-content">
+              
+              <ScholarshipApplicationContent />
+            </div>
           </div>
         </main>
       </div>
     </div>
   )
 }
+
+
 
